d3c2ae5b4632fcd886d399846036ec59c9357fce
Mahesh <jagtapmv991@gmail.com>
2022-09-29 14:51:09 +0530

    Created traverse method and added search method

--- LinkedList/singlyLL.go
+++ LinkedList/singlyLL.go
@@ -5,37 +5,68 @@ import "fmt"
 //create a node struct with data and next pointer
 
 type Node struct {
-	Data int
+	Data any
 	Next *Node
+}
+
+//Function to traverse the Linked List
+func (head *Node) traverseLL() {
+	data := head.Data
+	next := head.Next
+	i := 0
+	for {
+		fmt.Printf("%dth value is: %v\n", i, data)
+		data = next.Data
+		next = next.Next
+		i++
+		if next == nil {
+			fmt.Printf("%dth value is: %v\n", i, data)
+			break
+		}
+	}
+}
+
+func (head *Node) searchLL(searchTerm any) {
+	data := head.Data
+	next := head.Next
+	i := 0
+	flag := false
+	for {
+		if data == searchTerm {
+			flag = true
+			fmt.Println("Found at index: ", i)
+			break
+		}
+		if next == nil {
+			break
+		}
+		data = next.Data
+		next = next.Next
+		i++
+	}
+	if !flag {
+		fmt.Println("The given search term could not be found!")
+	}
 }
 
 func main() {
 	//creating 3 nodes with 0 as data and nil as pointer
-	head := Node{0, nil}
-	first := Node{0, nil}
-	second := Node{0, nil}
+	head := Node{nil, nil}
+	first := Node{nil, nil}
+	second := Node{nil, nil}
 
 	//assigning arbitrary value as data and linking the nodes by assining next node address
 	//to the pointer of previous node
 	head.Data = 1
 	head.Next = &first
-	first.Data = 2
+	first.Data = "b"
 	first.Next = &second
-	second.Data = 3
+	second.Data = 10.3
 
 	//Traversing through the list
-	data := head.Data
-	next := head.Next
-	i := 0
-	for {
-		fmt.Printf("%dth value is: %d\n", i, data)
-		data = next.Data
-		next = next.Next
-		i++
-		if next == nil {
-			fmt.Printf("%dth value is: %d\n", i, data)
-			break
-		}
-	}
+	head.traverseLL()
 	fmt.Println("List traversal is completed!")
+
+	//Search for specific search term
+	head.searchLL(1)
 }
